8632306baf6d6b636ae72b8fd837c862959af480
LATIsi <p.kmj9911@gmail.com>
2022-08-28 00:16:32 +0900

    1.0.6

--- js/app.js
+++ js/app.js
@@ -1,23 +1,62 @@
 const canvas =document.querySelector("canvas");
 const lineWidth =document.getElementById("line-width");
+
 const allColor =document.getElementById("allcolor");
 const jsMode =document.getElementById("jsMode");
 const colorOption =Array.from(document.getElementsByClassName("control_color"));
+
 const buttonClear = document.getElementById("jsClear");
 const buttonEraser = document.getElementById("jsEraser");
 
 const inputFile = document.getElementById("file");
+const inputText = document.getElementById("text");
+const saveButton = document.getElementById("jsSave");
+
+const selectFontFace = document.getElementById("fontSelect");
+const selectFontColor = document.getElementById("fontColor");
+const selectFontSize = document.getElementById("fontSize");
+
+
+
 const canvasHeight = 1000;
 const canvasWidth  = 1000;
 
 //3d or 2d
 const ctx = canvas.getContext("2d");
+
 canvas.width = 800;
 canvas.height = 400;
+
+//폰트
+let font_Alumni = new FontFace('Alumni Sans Pinstripe','url("https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe&display=swap")');
+
+let font_Roboto = new FontFace('Roboto','url("https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe&family=Roboto:wght@300&display=swap")');
+
+let font_Nanum = new FontFace('Nanum','url("https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe&family=Nanum+Myeongjo&display=swap")');
+
+let font_Silkscreen = new FontFace('Silkscreen','url("https://fonts.googleapis.com/css2?family=Alumni+Sans+Pinstripe&family=Silkscreen&display=swap")');
+
+
+font_Alumni.load().then(function(){
+ ctx.font = 'Alumni Sans Pinstripe';
+});
+
+font_Roboto.load().then(function(){
+    ctx.font = 'Roboto';
+});
+
+font_Nanum.load().then(function(){
+    ctx.font = 'Nanum';
+});
+
+font_Silkscreen.load().then(function(){
+    ctx.font = 'Silkscreen';
+});
 
 
 //html이 먼저 load 되어서 값을 가져올수있음.
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 
 
 
@@ -59,6 +98,8 @@ ctx.lineWidth = 1;
 let isPainting = false;
 let isFilling = false;
 
+
+
 function onClickMove(e){
     if (isPainting) {
         ctx.lineTo(e.offsetX,e.offsetY);
@@ -81,6 +122,10 @@ function cancleDraw(){
 
 function onLineWidthChange(e){
     ctx.lineWidth = e.target.value;
+}
+
+function onFontSizeChange(e){
+    ctx.font = e.target.value;
 }
 
 function onColorChange(e){
@@ -124,13 +169,23 @@ function onClear(){
     ctx.fillRect(0,0,canvasWidth,canvasHeight);
 }
 
-function onEraser(){
-    ctx.strokeStyle = "white";
-    isFilling = false;
+
+
+
+function onDoubleClick(e){
+   
+   const text = inputText.value;
+   if(text !== ""){
+        ctx.save();
+        ctx.lineWidth = 2;
+        ctx.fillText(text,e.offsetX,e.offsetY);
+        ctx.restore();
+   }
 }
 
 
 function onFileChange(e){
+    // 선택하는 이미지가 하나라 0번째것만 가져온다.
     const file = e.target.files[0];
     const url = URL.createObjectURL(file);
     const image = new Image();
@@ -145,12 +200,25 @@ function onFileChange(e){
 
 }
 
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a  = document.createElement("a");
+
+    a.href = url;
+    a.download = "myDrawing.png";
+    a.click();
+}
 
 // canvas.addEventListener("mousemove", onClickCanvas);
 canvas.addEventListener("mousemove", onClickMove);
 canvas.addEventListener("mousedown", startDraw);
 canvas.addEventListener("mouseup", cancleDraw);
 canvas.addEventListener("mouseleave", cancleDraw);
+
+
+canvas.addEventListener("dblclick",
+onDoubleClick);
+
 
 lineWidth.addEventListener("change",onLineWidthChange);
 allColor.addEventListener("change",onColorChange)
@@ -162,8 +230,25 @@ colorOption.forEach((color)=>{
 jsMode.addEventListener("click",onClickMode);
 canvas.addEventListener("mousedown", onFillCanvas);
 
-buttonEraser.addEventListener("click",onEraser);
+buttonEraser.onClcik = function onEraser(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+};
+
 buttonClear.addEventListener("click",onClear);
+
+
+//폰트
+selectFontSize.addEventListener("change",onFontSizeChange);
+
+
+
+
+
+
+
+
 inputFile.addEventListener("change", onFileChange);
 
+saveButton.addEventListener("click", onSaveClick);
 
